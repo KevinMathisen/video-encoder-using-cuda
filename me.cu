51a57f1c490df0c29e2cc3e26fa5fa0eda7d4596
Kevin N. Mathisen <kevin.nikolai.mathisen@gmail.com>
2025-02-11 18:58:44 +0100

    Fix delaration of vars in same scope by adding blocks

--- me.cu
+++ me.cu
@@ -24,103 +24,109 @@ void c63_motion_estimate(struct c63_common *cm)
   /* Compare this frame with previous reconstructed frame */
   
   /* Motion estimation for Luma (Y) */
-  // Variables needed for Y
-  int w = cm->padw[Y_COMPONENT], h = cm->padh[Y_COMPONENT];
-  int cols = cm->mb_cols, rows = cm->mb_rows;
-  int range = cm->me_search_range;
+  {
+    // Variables needed for Y
+    int w = cm->padw[Y_COMPONENT], h = cm->padh[Y_COMPONENT];
+    int cols = cm->mb_cols, rows = cm->mb_rows;
+    int range = cm->me_search_range;
 
-  uint8_t *d_orig_y = NULL;
-  uint8_t *d_ref_y  = NULL;
-  struct macroblock *d_mbs_y = NULL;
+    uint8_t *d_orig_y = NULL;
+    uint8_t *d_ref_y  = NULL;
+    struct macroblock *d_mbs_y = NULL;
 
-  // Allocate and copy memory
-  cudaMalloc((void**)&d_orig_y, w*h*sizeof(uint8_t));
-  cudaMalloc((void**)&d_ref_y, w*h*sizeof(uint8_t));
-  cudaMalloc((void**)&d_mbs_y, cols*rows*sizeof(struct macroblock));
+    // Allocate and copy memory
+    cudaMalloc((void**)&d_orig_y, w*h*sizeof(uint8_t));
+    cudaMalloc((void**)&d_ref_y, w*h*sizeof(uint8_t));
+    cudaMalloc((void**)&d_mbs_y, cols*rows*sizeof(struct macroblock));
 
-  cudaMemcpy(d_orig_y, cm->curframe->orig->Y, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_ref_y, cm->refframe->recons->Y, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_orig_y, cm->curframe->orig->Y, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_ref_y, cm->refframe->recons->Y, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
 
-  // Launch kernel
-  dim3 grid(cols, rows, 1);
-  dim3 block(2*range, 2*range, 1);
+    // Launch kernel
+    dim3 grid(cols, rows, 1);
+    dim3 block(2*range, 2*range, 1);
 
-  me_kernel<<<grid, block>>>(d_orig_y, d_ref_y, d_mbs_y, 
-    range, w, h, cols, rows);
-  cudaDeviceSynchronize();
+    me_kernel<<<grid, block>>>(d_orig_y, d_ref_y, d_mbs_y, 
+      range, w, h, cols, rows);
+    cudaDeviceSynchronize();
 
-  // Copy back results and free memory
-  cudaMemcpy(cm->curframe->mbs[Y_COMPONENT], d_mbs_y, cols*rows*sizeof(struct macroblock), cudaMemcpyDeviceToHost);
+    // Copy back results and free memory
+    cudaMemcpy(cm->curframe->mbs[Y_COMPONENT], d_mbs_y, cols*rows*sizeof(struct macroblock), cudaMemcpyDeviceToHost);
 
-  cudaFree(d_orig_y);
-  cudaFree(d_ref_y);
-  cudaFree(d_mbs_y);
+    cudaFree(d_orig_y);
+    cudaFree(d_ref_y);
+    cudaFree(d_mbs_y);
+  }
 
   /* Motion estimation for Chroma (U) */
-  // Variables needed for U
-  int w = cm->padw[U_COMPONENT], h = cm->padh[U_COMPONENT];
-  int cols = cm->mb_cols/2, rows = cm->mb_rows/2;
-  int range = cm->me_search_range/2;
+  {
+    // Variables needed for U
+    int w = cm->padw[U_COMPONENT], h = cm->padh[U_COMPONENT];
+    int cols = cm->mb_cols/2, rows = cm->mb_rows/2;
+    int range = cm->me_search_range/2;
 
-  uint8_t *d_orig_u = NULL;
-  uint8_t *d_ref_u  = NULL;
-  struct macroblock *d_mbs_u = NULL;
+    uint8_t *d_orig_u = NULL;
+    uint8_t *d_ref_u  = NULL;
+    struct macroblock *d_mbs_u = NULL;
 
-  // Allocate and copy memory
-  cudaMalloc((void**)&d_orig_u, w*h*sizeof(uint8_t));
-  cudaMalloc((void**)&d_ref_u, w*h*sizeof(uint8_t));
-  cudaMalloc((void**)&d_mbs_u, cols*rows*sizeof(struct macroblock));
+    // Allocate and copy memory
+    cudaMalloc((void**)&d_orig_u, w*h*sizeof(uint8_t));
+    cudaMalloc((void**)&d_ref_u, w*h*sizeof(uint8_t));
+    cudaMalloc((void**)&d_mbs_u, cols*rows*sizeof(struct macroblock));
 
-  cudaMemcpy(d_orig_u, cm->curframe->orig->U, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_ref_u, cm->refframe->recons->U, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_orig_u, cm->curframe->orig->U, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_ref_u, cm->refframe->recons->U, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
 
-  // Launch kernel
-  dim3 grid(cols, rows, 1);
-  dim3 block(2*range, 2*range, 1);
+    // Launch kernel
+    dim3 grid(cols, rows, 1);
+    dim3 block(2*range, 2*range, 1);
 
-  me_kernel<<<grid, block>>>(d_orig_u, d_ref_u, d_mbs_u, 
-    range, w, h, cols, rows);
-  cudaDeviceSynchronize();
+    me_kernel<<<grid, block>>>(d_orig_u, d_ref_u, d_mbs_u, 
+      range, w, h, cols, rows);
+    cudaDeviceSynchronize();
 
-  // Copy back results and free memory
-  cudaMemcpy(cm->curframe->mbs[U_COMPONENT], d_mbs_u, cols*rows*sizeof(struct macroblock), cudaMemcpyDeviceToHost);
+    // Copy back results and free memory
+    cudaMemcpy(cm->curframe->mbs[U_COMPONENT], d_mbs_u, cols*rows*sizeof(struct macroblock), cudaMemcpyDeviceToHost);
 
-  cudaFree(d_orig_u);
-  cudaFree(d_ref_u);
-  cudaFree(d_mbs_u);
+    cudaFree(d_orig_u);
+    cudaFree(d_ref_u);
+    cudaFree(d_mbs_u);
+  }
 
   /* Motion estimation for Chroma (V) */
-  // Variables needed for V
-  int w = cm->padw[V_COMPONENT], h = cm->padh[V_COMPONENT];
-  int cols = cm->mb_cols/2, rows = cm->mb_rows/2;
-  int range = cm->me_search_range/2;
+  {
+    // Variables needed for V
+    int w = cm->padw[V_COMPONENT], h = cm->padh[V_COMPONENT];
+    int cols = cm->mb_cols/2, rows = cm->mb_rows/2;
+    int range = cm->me_search_range/2;
 
-  uint8_t *d_orig_v = NULL;
-  uint8_t *d_ref_v  = NULL;
-  struct macroblock *d_mbs_v = NULL;
+    uint8_t *d_orig_v = NULL;
+    uint8_t *d_ref_v  = NULL;
+    struct macroblock *d_mbs_v = NULL;
 
-  // Allocate and copy memory
-  cudaMalloc((void**)&d_orig_v, w*h*sizeof(uint8_t));
-  cudaMalloc((void**)&d_ref_v, w*h*sizeof(uint8_t));
-  cudaMalloc((void**)&d_mbs_v, cols*rows*sizeof(struct macroblock));
+    // Allocate and copy memory
+    cudaMalloc((void**)&d_orig_v, w*h*sizeof(uint8_t));
+    cudaMalloc((void**)&d_ref_v, w*h*sizeof(uint8_t));
+    cudaMalloc((void**)&d_mbs_v, cols*rows*sizeof(struct macroblock));
 
-  cudaMemcpy(d_orig_v, cm->curframe->orig->V, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_ref_v, cm->refframe->recons->V, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_orig_v, cm->curframe->orig->V, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_ref_v, cm->refframe->recons->V, w*h*sizeof(uint8_t), cudaMemcpyHostToDevice);
 
-  // Launch kernel
-  dim3 grid(cols, rows, 1);
-  dim3 block(2*range, 2*range, 1);
+    // Launch kernel
+    dim3 grid(cols, rows, 1);
+    dim3 block(2*range, 2*range, 1);
 
-  me_kernel<<<grid, block>>>(d_orig_v, d_ref_v, d_mbs_v, 
-    range, w, h, cols, rows);
-  cudaDeviceSynchronize();
+    me_kernel<<<grid, block>>>(d_orig_v, d_ref_v, d_mbs_v, 
+      range, w, h, cols, rows);
+    cudaDeviceSynchronize();
 
-  // Copy back results and free memory
-  cudaMemcpy(cm->curframe->mbs[V_COMPONENT], d_mbs_v, cols*rows*sizeof(struct macroblock), cudaMemcpyDeviceToHost);
+    // Copy back results and free memory
+    cudaMemcpy(cm->curframe->mbs[V_COMPONENT], d_mbs_v, cols*rows*sizeof(struct macroblock), cudaMemcpyDeviceToHost);
 
-  cudaFree(d_orig_v);
-  cudaFree(d_ref_v);
-  cudaFree(d_mbs_v);
+    cudaFree(d_orig_v);
+    cudaFree(d_ref_v);
+    cudaFree(d_mbs_v);
+  }
 
 }
 
