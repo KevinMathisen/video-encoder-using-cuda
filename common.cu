183a50d1affad40544463f25f20a0ad5e61a5680
Kevin N. Mathisen <kevin.nikolai.mathisen@gmail.com>
2025-02-16 19:27:21 +0100

    Add pinned memory to predicted and motion vectors

--- common.cu
+++ common.cu
@@ -7,7 +7,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <cuda_runtime.h>
+
 #include "common.h"
+
+#define CUDA_CHECK(call)                                                    \
+  do {                                                                      \
+    cudaError_t err = call;                                                \
+    if (err != cudaSuccess) {                                              \
+      fprintf(stderr, "CUDA error %s:%d: %s\n",                             \
+              __FILE__, __LINE__, cudaGetErrorString(err));                \
+      exit(1);                                                             \
+    }                                                                      \
+  } while (0)
 
 void destroy_frame(struct frame *f)
 {
@@ -24,14 +36,16 @@ void destroy_frame(struct frame *f)
   free(f->residuals->Vdct);
   free(f->residuals);
 
-  free(f->predicted->Y);
-  free(f->predicted->U);
-  free(f->predicted->V);
+  // Free pinned memory
+  CUDA_CHECK(cudaFreeHost(f->predicted->Y));
+  CUDA_CHECK(cudaFreeHost(f->predicted->U));
+  CUDA_CHECK(cudaFreeHost(f->predicted->V));
   free(f->predicted);
 
-  free(f->mbs[Y_COMPONENT]);
-  free(f->mbs[U_COMPONENT]);
-  free(f->mbs[V_COMPONENT]);
+  // Free pinned memory
+  CUDA_CHECK(cudaFreeHost(f->mbs[Y_COMPONENT]));
+  CUDA_CHECK(cudaFreeHost(f->mbs[U_COMPONENT]));
+  CUDA_CHECK(cudaFreeHost(f->mbs[V_COMPONENT]));
 
   free(f);
 }
@@ -42,27 +56,27 @@ struct frame* create_frame(struct c63_common *cm, yuv_t *image)
 
   f->orig = image;
 
+  // Use pinned memory for reconstructed, as this will be used to encode next frame
   f->recons = (yuv_t*)malloc(sizeof(yuv_t));
   f->recons->Y = (uint8_t*)malloc(cm->ypw * cm->yph);
   f->recons->U = (uint8_t*)malloc(cm->upw * cm->uph);
   f->recons->V = (uint8_t*)malloc(cm->vpw * cm->vph);
 
+  // Use pinned memory for predicted, as this will be written to from the GPU
   f->predicted = (yuv_t*)malloc(sizeof(yuv_t));
-  f->predicted->Y = (uint8_t*)calloc(cm->ypw * cm->yph, sizeof(uint8_t));
-  f->predicted->U = (uint8_t*)calloc(cm->upw * cm->uph, sizeof(uint8_t));
-  f->predicted->V = (uint8_t*)calloc(cm->vpw * cm->vph, sizeof(uint8_t));
+  CUDA_CHECK(cudaHostAlloc((void**)&(f->predicted->Y), cm->ypw * cm->yph * sizeof(uint8_t), cudaHostAllocDefault));
+  CUDA_CHECK(cudaHostAlloc((void**)&(f->predicted->U), cm->upw * cm->uph * sizeof(uint8_t), cudaHostAllocDefault));
+  CUDA_CHECK(cudaHostAlloc((void**)&(f->predicted->V), cm->vpw * cm->vph * sizeof(uint8_t), cudaHostAllocDefault));
 
   f->residuals = (dct_t*)malloc(sizeof(dct_t));
   f->residuals->Ydct = (int16_t*)calloc(cm->ypw * cm->yph, sizeof(int16_t));
   f->residuals->Udct = (int16_t*)calloc(cm->upw * cm->uph, sizeof(int16_t));
   f->residuals->Vdct = (int16_t*)calloc(cm->vpw * cm->vph, sizeof(int16_t));
 
-  f->mbs[Y_COMPONENT] =
-    (macroblock*)calloc(cm->mb_rows * cm->mb_cols, sizeof(struct macroblock));
-  f->mbs[U_COMPONENT] =
-    (macroblock*)calloc(cm->mb_rows/2 * cm->mb_cols/2, sizeof(struct macroblock));
-  f->mbs[V_COMPONENT] =
-    (macroblock*)calloc(cm->mb_rows/2 * cm->mb_cols/2, sizeof(struct macroblock));
+  // Use pinned memory for motion vectors, as this will be written to from the GPU
+  CUDA_CHECK(cudaHostAlloc((void**)&(f->mbs[Y_COMPONENT]), cm->mb_rows * cm->mb_cols * sizeof(struct macroblock), cudaHostAllocDefault));
+  CUDA_CHECK(cudaHostAlloc((void**)&(f->mbs[U_COMPONENT]), cm->mb_rows/2 * cm->mb_cols/2 * sizeof(struct macroblock), cudaHostAllocDefault));
+  CUDA_CHECK(cudaHostAlloc((void**)&(f->mbs[V_COMPONENT]), cm->mb_rows/2 * cm->mb_cols/2 * sizeof(struct macroblock), cudaHostAllocDefault));
 
   return f;
 }
