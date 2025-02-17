33c80ca2e692efa459170bca709564eb8dacbb61
Mohammad Fadel Al Khafaji <101406844+Fadel-2000@users.noreply.github.com>
2025-02-17 16:13:08 +0100

    Update me.cu with better error checking for kernels

--- me.cu
+++ me.cu
@@ -16,6 +16,16 @@
 
 #include <cuda_runtime.h>
 
+#define CUDA_CHECK(call)                                                    \
+  do {                                                                      \
+    cudaError_t err = call;                                                \
+    if (err != cudaSuccess) {                                              \
+      fprintf(stderr, "CUDA error %s:%d: %s\n",                             \
+              __FILE__, __LINE__, cudaGetErrorString(err));                \
+      exit(1);                                                             \
+    }                                                                      \
+  } while (0)
+
 /* Global Variables */
 int w_y, h_y, w_uv, h_uv;                 // Height and width
 int mb_cols_y, mb_rows_y, mb_cols_uv, mb_rows_uv; // Columns and rows
@@ -56,42 +66,42 @@ __host__ void gpu_init(struct c63_common *cm)
   range = cm->me_search_range;
 
   // Allocate memory for input
-  cudaMallocManaged((void **) &d_in_org_Y, mem_size_y);
-  cudaMallocManaged((void **) &d_in_org_U, mem_size_uv);
-  cudaMallocManaged((void **) &d_in_org_V, mem_size_uv);
+  CUDA_CHECK(cudaMallocManaged((void **) &d_in_org_Y, mem_size_y));
+  CUDA_CHECK(cudaMallocManaged((void **) &d_in_org_U, mem_size_uv));
+  CUDA_CHECK(cudaMallocManaged((void **) &d_in_org_V, mem_size_uv));
 
-  cudaMallocManaged((void **) &d_in_ref_Y, mem_size_y);
-  cudaMallocManaged((void **) &d_in_ref_U, mem_size_uv);
-  cudaMallocManaged((void **) &d_in_ref_V, mem_size_uv);
+  CUDA_CHECK(cudaMallocManaged((void **) &d_in_ref_Y, mem_size_y));
+  CUDA_CHECK(cudaMallocManaged((void **) &d_in_ref_U, mem_size_uv));
+  CUDA_CHECK(cudaMallocManaged((void **) &d_in_ref_V, mem_size_uv));
 
   // Allocate memory for macroblock offsets
-  cudaMallocManaged((void**) &d_mbs_Y, mem_size_mbs_y);
-  cudaMallocManaged((void**) &d_mbs_U, mem_size_mbs_uv);
-  cudaMallocManaged((void**) &d_mbs_V, mem_size_mbs_uv);
+  CUDA_CHECK(cudaMallocManaged((void**) &d_mbs_Y, mem_size_mbs_y));
+  CUDA_CHECK(cudaMallocManaged((void**) &d_mbs_U, mem_size_mbs_uv));
+  CUDA_CHECK(cudaMallocManaged((void**) &d_mbs_V, mem_size_mbs_uv));
 
   // Allocate memory for output
-  cudaMallocManaged((void**) &d_out_Y, mem_size_y);
-  cudaMallocManaged((void**) &d_out_U, mem_size_uv);
-  cudaMallocManaged((void**) &d_out_V, mem_size_uv);
+  CUDA_CHECK(cudaMallocManaged((void**) &d_out_Y, mem_size_y));
+  CUDA_CHECK(cudaMallocManaged((void**) &d_out_U, mem_size_uv));
+  CUDA_CHECK(cudaMallocManaged((void**) &d_out_V, mem_size_uv));
 }
 
 __host__ void gpu_cleanup()
 {
-  cudaFree(d_in_org_Y);
-  cudaFree(d_in_org_U);
-  cudaFree(d_in_org_V);
+  CUDA_CHECK(cudaFree(d_in_org_Y));
+  CUDA_CHECK(cudaFree(d_in_org_U));
+  CUDA_CHECK(cudaFree(d_in_org_V));
 
-  cudaFree(d_in_ref_Y);
-  cudaFree(d_in_ref_U);
-  cudaFree(d_in_ref_V);
+  CUDA_CHECK(cudaFree(d_in_ref_Y));
+  CUDA_CHECK(cudaFree(d_in_ref_U));
+  CUDA_CHECK(cudaFree(d_in_ref_V));
 
-  cudaFree(d_mbs_Y);
-  cudaFree(d_mbs_U);
-  cudaFree(d_mbs_V);
+  CUDA_CHECK(cudaFree(d_mbs_Y));
+  CUDA_CHECK(cudaFree(d_mbs_U));
+  CUDA_CHECK(cudaFree(d_mbs_V));
 
-  cudaFree(d_out_Y);
-  cudaFree(d_out_U);
-  cudaFree(d_out_V);
+  CUDA_CHECK(cudaFree(d_out_Y));
+  CUDA_CHECK(cudaFree(d_out_U));
+  CUDA_CHECK(cudaFree(d_out_V));
 }
 
 __host__ void c63_motion_estimate(struct c63_common *cm)
@@ -99,12 +109,12 @@ __host__ void c63_motion_estimate(struct c63_common *cm)
   /* Compare this frame with previous reconstructed frame */
   
   // Copy data to device
-  cudaMemcpy(d_in_org_Y, cm->curframe->orig->Y, mem_size_y, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_in_ref_Y, cm->refframe->recons->Y, mem_size_y, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_in_org_U, cm->curframe->orig->U, mem_size_uv, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_in_ref_U, cm->refframe->recons->U, mem_size_uv, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_in_org_V, cm->curframe->orig->V, mem_size_uv, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_in_ref_V, cm->refframe->recons->V, mem_size_uv, cudaMemcpyHostToDevice);
+  CUDA_CHECK(cudaMemcpy(d_in_org_Y, cm->curframe->orig->Y, mem_size_y, cudaMemcpyHostToDevice));
+  CUDA_CHECK(cudaMemcpy(d_in_ref_Y, cm->refframe->recons->Y, mem_size_y, cudaMemcpyHostToDevice));
+  CUDA_CHECK(cudaMemcpy(d_in_org_U, cm->curframe->orig->U, mem_size_uv, cudaMemcpyHostToDevice));
+  CUDA_CHECK(cudaMemcpy(d_in_ref_U, cm->refframe->recons->U, mem_size_uv, cudaMemcpyHostToDevice));
+  CUDA_CHECK(cudaMemcpy(d_in_org_V, cm->curframe->orig->V, mem_size_uv, cudaMemcpyHostToDevice));
+  CUDA_CHECK(cudaMemcpy(d_in_ref_V, cm->refframe->recons->V, mem_size_uv, cudaMemcpyHostToDevice));
   
   // Set dimentions for grid and blocks
   dim3 block_grid_y(mb_cols_y, mb_rows_y, 1);
@@ -117,31 +127,72 @@ __host__ void c63_motion_estimate(struct c63_common *cm)
   me_kernel<<<block_grid_y, thread_grid_y>>>(d_in_org_Y, d_in_ref_Y, d_mbs_Y, 
   range, w_y, h_y, mb_cols_y, mb_rows_y);
 
+  cudaError_t err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    fprintf(stderr, "Kernel 1 launch error: %s\n", cudaGetErrorString(err));
+    exit(1);
+  }
+
+
   me_kernel<<<block_grid_uv, thread_grid_uv>>>(d_in_org_U, d_in_ref_U, d_mbs_U, 
   range/2, w_uv, h_uv, mb_cols_uv, mb_rows_uv);
+
+  err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    fprintf(stderr, "Kernel 2 launch error: %s\n", cudaGetErrorString(err));
+    exit(1);
+  }
 
   me_kernel<<<block_grid_uv, thread_grid_uv>>>(d_in_org_V, d_in_ref_V, d_mbs_V, 
   range/2, w_uv, h_uv, mb_cols_uv, mb_rows_uv);
 
+  err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    fprintf(stderr, "Kernel 3 launch error: %s\n", cudaGetErrorString(err));
+    exit(1);
+  }
+
+  // copy data back to host memory 
+  CUDA_CHECK(cudaMemcpy(cm->curframe->mbs[Y_COMPONENT], d_mbs_Y, mem_size_mbs_y, cudaMemcpyDeviceToHost));
+  CUDA_CHECK(cudaMemcpy(cm->curframe->mbs[U_COMPONENT], d_mbs_U, mem_size_mbs_uv, cudaMemcpyDeviceToHost));
+  CUDA_CHECK(cudaMemcpy(cm->curframe->mbs[V_COMPONENT], d_mbs_V, mem_size_mbs_uv, cudaMemcpyDeviceToHost));
 }
 
-void c63_motion_compensate(struct c63_common *cm)
+__host__ void c63_motion_compensate(struct c63_common *cm)
 {
-  dim3 block_grid_y(mb_cols_y, mb_rows_y);
-  dim3 block_grid_uv(mb_cols_uv, mb_rows_uv);
+  dim3 block_grid_y( cm->mb_cols, cm->mb_rows);
+  dim3 block_grid_uv( cm->mb_cols/2, cm->mb_rows/2);
 
   dim3 thread_grid(8, 8);
 
   mc_kernel<<<block_grid_y, thread_grid>>>(d_out_Y, d_in_ref_Y, d_mbs_Y, 
   w_y, h_y, mb_cols_y, mb_rows_y);
 
+  cudaError_t err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    fprintf(stderr, "Kernel 4 launch error: %s\n", cudaGetErrorString(err));
+    exit(1);
+  }
+
   mc_kernel<<<block_grid_uv, thread_grid>>>(d_out_U, d_in_ref_U, d_mbs_U, 
   w_uv, h_uv, mb_cols_uv, mb_rows_uv);
+
+  err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    fprintf(stderr, "Kernel 5 launch error: %s\n", cudaGetErrorString(err));
+    exit(1);
+  }
 
   mc_kernel<<<block_grid_uv, thread_grid>>>(d_out_V, d_in_ref_V, d_mbs_V, 
   w_uv, h_uv, mb_cols_uv, mb_rows_uv);
 
-  cudaMemcpy(cm->curframe->predicted->Y, d_out_Y, mem_size_y, cudaMemcpyDeviceToHost);
-  cudaMemcpy(cm->curframe->predicted->U, d_out_U, mem_size_uv, cudaMemcpyDeviceToHost);
-  cudaMemcpy(cm->curframe->predicted->V, d_out_V, mem_size_uv, cudaMemcpyDeviceToHost);
+  err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    fprintf(stderr, "Kernel 6 launch error: %s\n", cudaGetErrorString(err));
+    exit(1);
+  }
+
+  CUDA_CHECK(cudaMemcpy(cm->curframe->predicted->Y, d_out_Y, mem_size_y, cudaMemcpyDeviceToHost));
+  CUDA_CHECK(cudaMemcpy(cm->curframe->predicted->U, d_out_U, mem_size_uv, cudaMemcpyDeviceToHost));
+  CUDA_CHECK(cudaMemcpy(cm->curframe->predicted->V, d_out_V, mem_size_uv, cudaMemcpyDeviceToHost));
 }
